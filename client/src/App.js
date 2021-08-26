52469b95af9f359a32bb05227911ec977de81dc6
Charly <alctagne@gmail.com>
2021-08-26 18:52:15 -0400

    signup update

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,24 @@
 import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
-import SignIn from './components/SignIn'
+import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
+
 
 function App() {
   return (
     <div className="App">
-      <SignIn />
+    <Router>
+    <Switch>
+      <Route path='/' exact component={SignIn}/>
+      <Route path='/SignUp' exact component={SignUp}/>
+    </Switch>
+    </Router>
     </div>
   );
 }
+
+
 
 export default App;
