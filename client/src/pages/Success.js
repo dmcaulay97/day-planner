6577f92fd26a32b7c61099e7b60d7b1b443534d8
Dylan McAulay <dmcaulay97@gmail.com>
2021-09-02 17:33:51 -0400

    success style

--- client/src/pages/Success.js
+++ client/src/pages/Success.js
@@ -1,10 +1,31 @@
 import React, { useEffect } from 'react';
 import { useMutation } from '@apollo/client';
 // import { SET_SUBSCRIBED } from '../utils/mutations';
+import { Grid, Card } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
 
+const useStyles = makeStyles({
+  root: {
+    minWidth: 275,
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
 
 function Success() {
   // const [addOrder] = useMutation(SET_SUBSCRIBED);
+  const classes = useStyles();
 
   useEffect(() => {
     async function subscribe() {
@@ -17,11 +38,19 @@ function Success() {
   }, []);
 
   return (
-    <div>
-      <h1>Success!</h1>
-      <h2>Thank you for your purchase!</h2>
-      <h2>You will now be redirected to the home page</h2>
-    </div>
+    <Card className={classes.root} >
+      <CardContent>
+        <Typography variant="h5" component="h2">
+          Thank You For Subscribing
+        </Typography>
+        <Typography variant="h5" component="h2">
+          You are now a premium member
+        </Typography>
+        <Typography className={classes.pos} color="textSecondary">
+          You will be redireted back to Amazing Day Planner shortly...
+        </Typography>
+      </CardContent>
+    </Card>
   );
 }
 
